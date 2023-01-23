42160033c5075e54bf15f0535ec2ffc3e1db08d8
alexandrunst <nastase.alexandru100@gmail.com>
2023-01-23 16:17:31 +0000

    If - Else

--- Section 16 JavaScript Decision Making/app.js
+++ Section 16 JavaScript Decision Making/app.js
@@ -1,1 +1,28 @@
 console.log("HELLO FROM OUR FIRST JS FILE!!!!");
+
+// If
+if (1 + 1 === 2) {
+    console.log("MATH STILL WORKS!!!");
+}
+
+// If - else
+let random = Math.random();
+console.log(random);
+if (random < 0.5) {
+    console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
+} else {
+    console.log("YOUR NUMBER IS GREATER THAN 0.5!!!");
+}
+
+// If - else if - else
+let age = 55;
+console.log(age);
+if (age < 5) {
+    console.log("You are a baby");
+} else if (age < 10) {
+    console.log("You are a child");
+} else if (age < 65) {
+    console.log("You are an adult");
+} else {
+    console.log("You are a senior");
+}
