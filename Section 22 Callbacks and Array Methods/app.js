db50b440166703eb4687c61d18ca2b1204547150
alexandrunst <nastase.alexandru100@gmail.com>
2023-01-26 15:14:29 +0000

    forEach

---
+++ Section 22 Callbacks and Array Methods/app.js
@@ -0,0 +1,15 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function print(element) {
+    console.log(element);
+}
+
+numbers.forEach(print)
+
+//////
+
+numbers.forEach(function (el) {
+    console.log(el);
+})
+
+/////
