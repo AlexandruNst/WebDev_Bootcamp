30bf0f2f37c5ad6fd161ce56c852917b6d935abf
alexandrunst <nastase.alexandru100@gmail.com>
2023-01-26 22:15:35 +0000

    REST Parameters

--- Section 23 Newer JavaScript Features/app.js
+++ Section 23 Newer JavaScript Features/app.js
@@ -11,3 +11,15 @@ const allPets = [...cats, ...dogs];
 
 const feline = { legs: 4, family: "Felidae" }
 const copyCat = { ...feline, isPet: true };
+
+///
+
+function sum(...nums) {
+    return nums.reduce((total, el) => total + el);
+}
+
+function race(first, second, ...rest) {
+    console.log(first);
+    console.log(second);
+    console.log(rest);
+}
