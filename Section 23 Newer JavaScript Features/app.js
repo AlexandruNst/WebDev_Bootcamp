26fa83192d9539007d0d82bb22a65e5eb5a6b8ae
alexandrunst <nastase.alexandru100@gmail.com>
2023-01-26 22:25:43 +0000

    Destructuring

--- Section 23 Newer JavaScript Features/app.js
+++ Section 23 Newer JavaScript Features/app.js
@@ -23,3 +23,20 @@ function race(first, second, ...rest) {
     console.log(second);
     console.log(rest);
 }
+
+////
+
+const scores = [10, 20, 30, 40, 50]
+const [gold, silver, bronze] = scores;
+
+const user = {
+    firstName: "Harvey",
+    lastName: "Milk"
+}
+const { firstName, lastName } = user;
+const { firstName: usersFirstName } = user;
+const { born = 1970, name = firstName } = user;
+
+function fullName({ firstName, lastName }) {
+    console.log(`${firstName} ${lastName}`)
+}
