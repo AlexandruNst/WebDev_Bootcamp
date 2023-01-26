fd8548b2315ba6bc749c583aff9970d68f4ea48d
alexandrunst <nastase.alexandru100@gmail.com>
2023-01-26 22:08:46 +0000

    Spread

--- Section 23 Newer JavaScript Features/app.js
+++ Section 23 Newer JavaScript Features/app.js
@@ -1,3 +1,13 @@
 function rollDie(numSides = 6) {
     return Math.floor(Math.random() * numSides) + 1;
 }
+
+/// 
+
+const cats = ["Blue", "Scout", "Rocket"];
+const dogs = ["Rusty", "Wyatt"];
+
+const allPets = [...cats, ...dogs];
+
+const feline = { legs: 4, family: "Felidae" }
+const copyCat = { ...feline, isPet: true };
