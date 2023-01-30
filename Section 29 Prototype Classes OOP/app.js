671be9aebd85fa4e535ab36c0f33fa9acc286169
alexandrunst <nastase.alexandru100@gmail.com>
2023-01-30 22:24:34 +0000

    Constructor function

---
+++ Section 29 Prototype Classes OOP/app.js
@@ -0,0 +1,12 @@
+function Color(r, g, b) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+}
+
+Color.prototype.rgb = function () {
+    const { r, g, b } = this;
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+let color = new Color(255, 40, 100);
