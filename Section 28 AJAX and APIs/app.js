25007a5b115c5b756fcb3abd7124a5b9862b40f7
alexandrunst <nastase.alexandru100@gmail.com>
2023-01-30 15:47:34 +0000

    Axios

--- Section 28 AJAX and APIs/app.js
+++ Section 28 AJAX and APIs/app.js
@@ -1,75 +1,75 @@
-// fetch("https://swapi.dev/api/people/1")
-//     .then(res => {
-//         console.log("RESOLVED!!");
-//         console.log(res);
-//         return res.json();
-//     })
-//     .then(data => console.log(data))
-//     .catch(e => {
-//         console.log("REJECTED!!");
-//         console.log(e);
-//     })
+fetch("https://swapi.dev/api/people/1")
+    .then(res => {
+        console.log("RESOLVED!!");
+        console.log(res);
+        return res.json();
+    })
+    .then(data => console.log(data))
+    .catch(e => {
+        console.log("REJECTED!!");
+        console.log(e);
+    })
 
-// /////////
+/////////
 
-// fetch("https://swapi.dev/api/people/1")
-//     .then(res => {
-//         console.log("RESOLVED!!");
-//         console.log(res);
-//         return res.json();
-//     })
-//     .then(data => {
-//         console.log(data);
-//         return fetch("https://swapi.dev/api/people/2")
-//     })
-//     .then(res => {
-//         console.log("RESOLVED!!");
-//         console.log(res);
-//         return res.json();
-//     })
-//     .then(data => {
-//         console.log(data);
-//     })
-//     .catch(e => {
-//         console.log("REJECTED!!");
-//         console.log(e);
-//     })
+fetch("https://swapi.dev/api/people/1")
+    .then(res => {
+        console.log("RESOLVED!!");
+        console.log(res);
+        return res.json();
+    })
+    .then(data => {
+        console.log(data);
+        return fetch("https://swapi.dev/api/people/2")
+    })
+    .then(res => {
+        console.log("RESOLVED!!");
+        console.log(res);
+        return res.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(e => {
+        console.log("REJECTED!!");
+        console.log(e);
+    })
 
-// ///////////
+///////////
 
-// const loadSW = async () => {
-//     try {
-//         const res = await fetch("https://swapi.dev/api/people/1");
-//         const data = await res.json();
-//         console.log(data);
+const loadSW = async () => {
+    try {
+        const res = await fetch("https://swapi.dev/api/people/1");
+        const data = await res.json();
+        console.log(data);
 
-//         const res2 = await fetch("https://swapi.dev/api/people/2");
-//         const data2 = await res2.json();
-//         console.log(data2);
-//     } catch (e) {
-//         console.log("ERROR!!");
-//         console.log(e);
-//     }
+        const res2 = await fetch("https://swapi.dev/api/people/2");
+        const data2 = await res2.json();
+        console.log(data2);
+    } catch (e) {
+        console.log("ERROR!!");
+        console.log(e);
+    }
 
-// }
+}
 
-// loadSW();
+loadSW();
 
-///////////////////////// AXIOS
+/////////////////////// AXIOS
 
-// axios.get("https://swapi.dev/api/people/1")
-//     .then(res => {
-//         console.log(res.data);
-//     })
+axios.get("https://swapi.dev/api/people/1")
+    .then(res => {
+        console.log(res.data);
+    })
 
-// // OR
+// OR
 
-// const loadAxiosSW = async () => {
-//     const res = await axios.get("https://swapi.dev/api/people/1");
-//     console.log(res.data);
-// }
+const loadAxiosSW = async () => {
+    const res = await axios.get("https://swapi.dev/api/people/1");
+    console.log(res.data);
+}
 
-// loadAxiosSW();
+loadAxiosSW();
 
 const jokes = document.querySelector("#jokes");
 const button = document.querySelector("button");
