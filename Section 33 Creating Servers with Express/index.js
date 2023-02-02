947d810b46a6b0e4dc6111fe91d173f445ea4ef6
alexandrunst <nastase.alexandru100@gmail.com>
2023-02-02 21:43:22 +0000

    Query parameters

--- Section 33 Creating Servers with Express/index.js
+++ Section 33 Creating Servers with Express/index.js
@@ -5,16 +5,17 @@ const app = express();
 // dogs => woof
 // /
 
+app.get("/", (req, res) => {
+    // res.send("THIS IS THE HOMEPAGE!");
+    res.send("WELCOME TO THE HOMEPAGE!");
+})
+
 app.get("/cats", (req, res) => {
     res.send("MEOW!");
 })
 
 app.get("/dogs", (req, res) => {
     res.send("WOOF!");
-})
-
-app.get("/", (req, res) => {
-    res.send("THIS IS THE HOMEPAGE!");
 })
 
 app.post("/cats", (req, res) => {
@@ -28,6 +29,11 @@ app.get("/r/:subreddit", (req, res) => {
 app.get("/r/:subreddit/:postId", (req, res) => {
     const { subreddit, postId } = req.params;
     res.send(`THIS IS THE ${postId} POST OF THE ${subreddit} SUBREDDIT!`)
+})
+
+app.get("/search", (req, res) => {
+    const { q } = req.query;
+    res.send(`<h1>Search results for: ${q}<h1/>`);
 })
 
 app.get("*", (req, res) => {
