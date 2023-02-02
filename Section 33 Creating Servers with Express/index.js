4f2ff1c0a8eb901e4ec7d290e795cd6205e7b894
alexandrunst <nastase.alexandru100@gmail.com>
2023-02-02 21:22:24 +0000

    Express Routing and Parameters

---
+++ Section 33 Creating Servers with Express/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+
+// cats => meow
+// dogs => woof
+// /
+
+app.get("/cats", (req, res) => {
+    res.send("MEOW!");
+})
+
+app.get("/dogs", (req, res) => {
+    res.send("WOOF!");
+})
+
+app.get("/", (req, res) => {
+    res.send("THIS IS THE HOMEPAGE!");
+})
+
+app.post("/cats", (req, res) => {
+    res.send("POST REQUEST TO /CATS")
+})
+
+app.get("/r/:subreddit", (req, res) => {
+    res.send(`THIS IS THE ${req.params.subreddit} SUBREDDIT!`)
+})
+
+app.get("/r/:subreddit/:postId", (req, res) => {
+    const { subreddit, postId } = req.params;
+    res.send(`THIS IS THE ${postId} POST OF THE ${subreddit} SUBREDDIT!`)
+})
+
+app.get("*", (req, res) => {
+    res.send("I DON'T KNOW THAT PATH :/")
+})
+
+app.listen(3000, () => {
+    console.log("LISTENING ON PORT!!");
+})
