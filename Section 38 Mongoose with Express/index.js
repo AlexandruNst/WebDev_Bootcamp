869ea52dfae7e5f761d048b52f30554bf0435cad
alexandrunst <nastase.alexandru100@gmail.com>
2023-02-09 13:38:43 +0000

    Create

--- Section 38 Mongoose with Express/index.js
+++ Section 38 Mongoose with Express/index.js
@@ -12,6 +12,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded());
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
@@ -22,8 +23,19 @@ app.get("/products", async (req, res) => {
     res.render("products/index", { products });
 })
 
+app.get("/products/new", (req, res) => {
+    res.render("products/new")
+})
+
 app.get("/products/:id", async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
     res.render("products/show", { product })
 })
+
+app.post("/products", async (req, res) => {
+    const product = new Product(req.body);
+    await product.save();
+    res.redirect("/products");
+})
+
