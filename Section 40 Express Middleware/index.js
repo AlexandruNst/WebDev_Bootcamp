5d4ecc4b3c69bd24151733d4076fad50fff1b412
alexandrunst <nastase.alexandru100@gmail.com>
2023-02-12 14:42:23 +0000

    Express middleware basics

---
+++ Section 40 Express Middleware/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+app.listen(3000, () => console.log("Listening on port 3000"));
+
+// app.use is called on every single request, regardless of type or response
+// tell it to call the Morgan middleware
+app.use(morgan("tiny"));
+app.use((req, res, next) => {
+    console.log(req.method, req.path);
+    req.requestTime = Date.now();
+    next();
+})
+
+// This defines middleware on specific paths
+// this mathces ALL types of requests
+app.use("/dogs", (req, res, next) => {
+    console.log("I LOVE DOGS");
+    next();
+})
+
+app.get("/", (req, res) => {
+    res.send("Home :)");
+})
+
+app.get("/dogs", (req, res) => {
+    console.log(req.requestTime);
+    res.send("Woof Woof");
+})
+
+// This middleware will only run if nothing else is send before, i.e. nothing matched i.e. a 404
+app.use((req, res) => {
+    // res.send("NOT FOUND");
+    res.status(404).send("Not found"); // otherwise out req status will actually be 200 OK cus we techinally found something
+})
