047de5668758c90ffbd703033c8853cad7cd423a
alexandrunst <nastase.alexandru100@gmail.com>
2023-02-19 18:30:51 +0000

    Refactoring methods on the User model and Login middleware

--- Section 50 Authentication Bcrypt/Auth Demo/index.js
+++ Section 50 Authentication Bcrypt/Auth Demo/index.js
@@ -19,16 +19,24 @@ app.use(express.urlencoded());
 // to use sessions
 app.use(session({ secret: "not a good secret" }));
 
+const requireLogin = (req, res, next) => {
+    if (!req.session.user_id) {
+        res.redirect("/login");
+    } else {
+        next();
+    }
+}
+
 app.get("/", (req, res) => {
     res.send("Home page");
 })
 
-app.get("/secret", (req, res) => {
-    if (req.session.user_id) {
-        res.render("secret");
-    } else {
-        res.redirect("login");
-    }
+app.get("/secret", requireLogin, (req, res) => {
+    res.render("secret");
+})
+
+app.get("/topsecret", requireLogin, (req, res) => {
+    res.send("TOP SECRET")
 })
 
 app.get("/register", (req, res) => {
@@ -37,11 +45,7 @@ app.get("/register", (req, res) => {
 
 app.post("/register", async (req, res) => {
     const { password, username } = req.body;
-    const hash = await bcrypt.hash(password, 12);
-    const user = new User({
-        username,
-        password: hash
-    });
+    const user = new User({ username, password });
     await user.save();
     req.session.user_id = user._id;
     res.redirect("/");
@@ -53,10 +57,9 @@ app.get("/login", (req, res) => {
 
 app.post("/login", async (req, res) => {
     const { password, username } = req.body;
-    const user = await User.findOne({ username });
-    const validPassword = await bcrypt.compare(password, user.password);
-    if (validPassword) {
-        req.session.user_id = user._id;
+    const foundUser = await User.findAndValidate(username, password);
+    if (foundUser) {
+        req.session.user_id = foundUser._id;
         res.redirect("/secret");
     } else {
         res.redirect("/login");
--- Section 50 Authentication Bcrypt/Auth Demo/models/user.js
+++ Section 50 Authentication Bcrypt/Auth Demo/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -9,6 +10,25 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, "Username cannot be blank"]
     }
+})
+
+userSchema.statics.findAndValidate = async function (username, password) {
+    // this refers to the schema/model
+    const foundUser = await this.findOne({ username });
+    const isValid = await bcrypt.compare(password, foundUser.password);
+    return isValid ? foundUser : false;
+}
+
+userSchema.pre("save", async function (next) {
+    // this refers to this particular user that we're saving
+    // we call user.save() -> that's why!
+
+    // this is there cus "save" is called when we update too
+    // on an update that is not for pw, we don't want to rehash the pw
+    if (!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
 })
 
 module.exports = mongoose.model("User", userSchema);
